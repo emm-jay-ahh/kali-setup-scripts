0b3fb1e6b7520a486e9a651800c752d0c3373391
emm-jay-ahh <84301086+emm-jay-ahh@users.noreply.github.com>
2024-05-08 21:00:20 +1000

    Update install_environment.sh

--- install_environment.sh
+++ install_environment.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
-# Define installation directory and GitHub repo
-INSTALL_DIR="$HOME/Scripts"
-REPO_URL="https://github.com/emm-jay-ahh/kali_setup_scripts.git"
+# Define installation directory based on the current directory
+INSTALL_DIR="$(pwd)"
 
-# Install git if not installed
-if ! command -v git &>/dev/null; then
-    echo "Git is not installed. Installing now..."
-    sudo apt-get update && sudo apt-get install git
-fi
-
-# Clone the repository or update if it already exists
-if [ ! -d "$INSTALL_DIR" ]; then
-    mkdir -p "$INSTALL_DIR"  # Ensure the directory exists
-    git clone "$REPO_URL" "$HOME/kali_setup_scripts" # Clone to a temporary directory
-    cp -r "$HOME/kali_setup_scripts/Scripts/"* "$INSTALL_DIR" # Copy only the Scripts folder
-    rm -rf "$HOME/kali_setup_scripts"  # Clean up temporary directory
-else
-    echo "Updating existing scripts"
-    # Pull and update approach
-    git clone "$REPO_URL" "$HOME/kali_setup_scripts" # Clone to a temporary directory
-    cp -r "$HOME/kali_setup_scripts/Scripts/"* "$INSTALL_DIR" # Update scripts
-    rm -rf "$HOME/kali_setup_scripts"  # Clean up temporary directory
-fi
-
-# Make scripts executable
-chmod +x "$INSTALL_DIR/set_ip.sh"
-chmod +x "$INSTALL_DIR/set_ip_completion.sh" # Adjust if Zsh completion script is different
+# Make sure scripts are executable
+echo "Setting executable permissions for scripts..."
+chmod +x "$INSTALL_DIR/Scripts/set_ip.sh"
+chmod +x "$INSTALL_DIR/Scripts/set_ip_completion.sh" # Adjust if Zsh completion script is different
 
 # Update .zshrc to include new functions and source completion script
 echo "Updating Zsh profile..."
@@ -36,7 +16,7 @@ if ! grep -Fxq "myipexport()" "$HOME/.zshrc"; then
 
 # Function to export IP
 myipexport() {
-    output=\$("$INSTALL_DIR/set_ip.sh" "\$1" 2>&1)
+    output=\$("$INSTALL_DIR/Scripts/set_ip.sh" "\$1" 2>&1)
     exit_status=\$?
     if [ \$exit_status -ne 0 ]; then
         echo "Error: \$output"
@@ -50,8 +30,8 @@ EOF
 fi
 
 # Check and source the IP address completion script if not already sourced
-if ! grep -Fxq "source \"$INSTALL_DIR/set_ip_completion.sh\"" "$HOME/.zshrc"; then
-    echo "source \"$INSTALL_DIR/set_ip_completion.sh\"" >> "$HOME/.zshrc"
+if ! grep -Fxq "source \"$INSTALL_DIR/Scripts/set_ip_completion.sh\"" "$HOME/.zshrc"; then
+    echo "source \"$INSTALL_DIR/Scripts/set_ip_completion.sh\"" >> "$HOME/.zshrc"
 fi
 
 # Apply changes

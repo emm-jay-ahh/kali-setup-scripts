3f0d71a54e163fdd6f503cf42aab4236687bbebd
emm-jay-ahh <84301086+emm-jay-ahh@users.noreply.github.com>
2024-05-08 21:03:09 +1000

    Update install_environment.sh

--- install_environment.sh
+++ install_environment.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Define installation directory based on the current directory
-INSTALL_DIR="$(pwd)"
+# Define the installation directory explicitly
+INSTALL_DIR="$HOME/Scripts"
+
+# Ensure the installation directory exists
+mkdir -p "$INSTALL_DIR"
 
 # Make sure scripts are executable
 echo "Setting executable permissions for scripts..."
-chmod +x "$INSTALL_DIR/Scripts/set_ip.sh"
-chmod +x "$INSTALL_DIR/Scripts/set_ip_completion.sh" # Adjust if Zsh completion script is different
+chmod +x "$INSTALL_DIR/set_ip.sh"
+chmod +x "$INSTALL_DIR/set_ip_completion.sh"  # Adjust if Zsh completion script is different
 
 # Update .zshrc to include new functions and source completion script
 echo "Updating Zsh profile..."
@@ -16,7 +19,7 @@ if ! grep -Fxq "myipexport()" "$HOME/.zshrc"; then
 
 # Function to export IP
 myipexport() {
-    output=\$("$INSTALL_DIR/Scripts/set_ip.sh" "\$1" 2>&1)
+    output=\$("$INSTALL_DIR/set_ip.sh" "\$1" 2>&1)
     exit_status=\$?
     if [ \$exit_status -ne 0 ]; then
         echo "Error: \$output"
@@ -30,8 +33,8 @@ EOF
 fi
 
 # Check and source the IP address completion script if not already sourced
-if ! grep -Fxq "source \"$INSTALL_DIR/Scripts/set_ip_completion.sh\"" "$HOME/.zshrc"; then
-    echo "source \"$INSTALL_DIR/Scripts/set_ip_completion.sh\"" >> "$HOME/.zshrc"
+if ! grep -Fxq "source \"$INSTALL_DIR/set_ip_completion.sh\"" "$HOME/.zshrc"; then
+    echo "source \"$INSTALL_DIR/set_ip_completion.sh\"" >> "$HOME/.zshrc"
 fi
 
 # Apply changes
